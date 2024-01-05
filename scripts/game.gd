6ca461eb1eb357de457c46ff676f1f45b1948cd9
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-05 17:24:09 +0000

    Startup and update score/lives

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,12 +3,18 @@ extends Node2D
 var lives = 3
 var score = 0
 @onready var player = $Player
+@onready var hud = $UI/HUD
+
+func _ready():
+	hud.set_score_label(score)
+	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area):
 	area.die()
 
 func _on_player_took_damage():
 	lives -= 1
+	hud.set_lives(lives)
 	if lives == 0:
 		player.die() 
 	
@@ -18,3 +24,4 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 
 func _on_enemy_died():
 	score += 10
+	hud.set_score_label(score)
