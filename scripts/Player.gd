dd895ea9bac073bf1393c6ee1687ebe3ac0b6d48
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-05 10:10:13 +0000

    Contrict Player movement to canvas size

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var SPEED = 250
 
 func _physics_process(delta):
-	velocity = Vector2(0, 0)
+	velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed('right'):
 		velocity.x = SPEED
@@ -15,3 +15,8 @@ func _physics_process(delta):
 		velocity.y = SPEED
 	
 	move_and_slide()
+	
+	# Constrict player to resolution size
+	var screen_size = get_viewport_rect().size  # Get current screen resolution (Vector2)
+	global_position = global_position.clamp(Vector2(0, 0), screen_size) 
+	
