69cf150e92afd80bdbfc17ba551b7f9e814c4c27
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-05 11:44:38 +0000

    Added container to fix spawn issue
    Rocket's movement was influenced by player movement. Fixed by adding rockets into a Node container. Node has no transform properties.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 var SPEED = 250
 var rocket_scene = preload('res://scenes/rocket.tscn')
+var rocket_container 
+
+func _ready():
+	rocket_container = get_node('RocketContainer')
 
 func _process(delta):
 	if Input.is_action_just_pressed('shoot'):
@@ -27,5 +31,6 @@ func _physics_process(delta):
 	
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
