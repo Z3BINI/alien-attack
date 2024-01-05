e6b29ba6353b477d48f4d892700ca63c07a9d65a
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-05 09:46:37 +0000

    Added Player movement logic

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,3 +1,17 @@
 extends CharacterBody2D
 
+var SPEED = 250
 
+func _physics_process(delta):
+	velocity = Vector2(0, 0)
+	
+	if Input.is_action_pressed('right'):
+		velocity.x = SPEED
+	if Input.is_action_pressed('left'):
+		velocity.x = -SPEED
+	if Input.is_action_pressed('up'):
+		velocity.y = -SPEED
+	if Input.is_action_pressed('down'):
+		velocity.y = SPEED
+	
+	move_and_slide()
