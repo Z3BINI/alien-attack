26444cee9b39619865776c265372339c11ffa7f1
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-05 16:25:46 +0000

    Added 3 lives for player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal took_damage
+
 var SPEED = 250
 var rocket_scene = preload('res://scenes/rocket.tscn')
 @onready var rocket_container = $RocketContainer
@@ -33,4 +35,7 @@ func shoot():
 	rocket_instance.global_position.x += 80
 	
 func take_damage():
-	print('ouch!')
+	emit_signal('took_damage')
+	
+func die():
+	queue_free()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
+var lives = 3
+@onready var player = $Player
+
 func _on_deathzone_area_entered(area):
 	area.die()
+
+
+func _on_player_took_damage():
+	lives -= 1
+	if lives == 0:
+		player.die() 
+	
