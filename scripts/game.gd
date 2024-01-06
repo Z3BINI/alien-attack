c7ee237723dde34d843c2369e884867ca7ca2e78
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-06 10:06:57 +0000

    Connected game over scene to game

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var lives = 3
 var score = 0
+var game_over_scene = preload('res://scenes/game_over_screen.tscn')
 @onready var player = $Player
 @onready var hud = $UI/HUD
 
@@ -16,7 +17,12 @@ func _on_player_took_damage():
 	lives -= 1
 	hud.set_lives(lives)
 	if lives == 0:
-		player.die() 
+		var game_over_inst = game_over_scene.instantiate()
+		player.die()
+		await get_tree().create_timer(1.5).timeout 
+		game_over_inst.set_score(score)
+		$UI.add_child(game_over_inst)
+		
 	
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	enemy_instance.connect('died', _on_enemy_died)
